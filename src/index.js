bf37bbc6ceed61ada4de9c2fb0c3becf68adde70
SambhavSharma7781 <sambhav7717@gmail.com>
2026-02-10 22:57:24 +0530

    feat: add cookie-parser and cors middleware, implement app structure with error handling

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import app from "./app.js";
 
 dotenv.config({
     path: ".env"
 })
 
-connectDB();
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT, () => {
+        console.log(`Server is running on PORT: ${process.env.PORT}`);
+    })
+})
+.catch((error) => {
+    console.log("MONGODB CONNECTION FAILED!", error);
+    
+})
