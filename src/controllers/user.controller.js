b00d4bf74f39923533a19827708a46143a5eb7e6
SambhavSharma7781 <sambhav7717@gmail.com>
2026-02-19 21:50:00 +0530

    feat: add functionality to fetch user watch history with video owner details

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -392,6 +392,54 @@ const getUserChannelProfile = asyncHandler( async (req, res) => {
     .json(new ApiResponse(200, channel[0], "Channel profile fetched successfully!"))
 } )
 
+const getWatchHistory = asyncHandler( async (req, res) => {
+    const user = await User.aggregate([
+        {
+            $match: {
+                _id: mongoose.Types.ObjectId(req.user._id)
+            }
+        },
+        {
+            $lookup: {
+                from: "videos",
+                localField: "watchHistory",
+                foreignField: "_id",
+                as: "watchHistory",
+                pipeline: [
+                    {
+                        $lookup: {
+                            from: "users",
+                            localField: "owner",
+                            foreignField: "_id",
+                            as: "owner",
+                            pipeline: [
+                                {
+                                    $project: {
+                                        fullname: 1,
+                                        username: 1,
+                                        avatar: 1
+                                    }
+                                },
+                                {
+                                    $addFields: {
+                                        owner: {
+                                            $first: "$owner"
+                                        }
+                                    }
+                                }
+                            ]
+                        }
+                    }
+                ]
+            } 
+        }
+    ])
+
+    return res
+    .status(200)
+    .json(new ApiResponse(200, user[0].watchHistory, "User watch history fetched successfully!"))
+} )
+
 
 
 export { 
@@ -404,5 +452,6 @@ export {
     updateAccountDetails,
     updateUserAvatar,
     updateUserCoverImage,
-    getUserChannelProfile
+    getUserChannelProfile,
+    getWatchHistory
 }
