b8705e75710853c5870398aa24b6d7a898156170
SambhavSharma7781 <sambhav7717@gmail.com>
2026-02-23 00:36:08 +0530

    feat: expand video routes with additional CRUD operations and improved structure

--- src/routes/video.routes.js
+++ src/routes/video.routes.js
@@ -1,20 +1,31 @@
 import { Router } from "express";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 import { upload } from "../middlewares/multer.middleware.js";
-import { getAllVideos, publishAVideo } from "../controllers/video.controller.js";
+import { deleteVideo, getAllVideos, getVideoById, publishAVideo, togglePublishStatus, updateVideo } from "../controllers/video.controller.js";
 
 const router = Router();
 
 router.route("/videos").get(getAllVideos);
 
 router.route("/videos")
-.post(
-    verifyJWT,
-    upload.fields([
-        { name: "videoFile", maxCount: 1 },
-        { name: "thumbnail", maxCount: 1 }
-    ]),
-    publishAVideo
-);
+    .post(
+        verifyJWT,
+        upload.fields([
+            { name: "videoFile", maxCount: 1 },
+            { name: "thumbnail", maxCount: 1 }
+        ]),
+        publishAVideo
+    );
+
+router.route("/videos/:videoId").get(getVideoById);
+
+router.route("/videos/:videoId")
+    .patch(verifyJWT, upload.single("thumbnail"), updateVideo);
+
+router.route("/videos/:videoId")
+    .delete(verifyJWT, deleteVideo);
+
+router.route("/videos/toggle/:videoId")
+    .patch(verifyJWT, togglePublishStatus);
 
 export default router;
